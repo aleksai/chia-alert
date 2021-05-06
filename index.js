b5f3a140e55898d7ef01aa7f09ac8a7bfdd21f70
Alexei Pugachev <lenyapugachev@me.com>
2021-05-07 01:33:17 +0700

    Add total to console

--- index.js
+++ index.js
@@ -18,8 +18,11 @@ obserser.on("file-updated", log => {
 	const eligible = /([1-9]{1}[0-9]*) plots were/
 	const eligiblefound = log.message.match(eligible)
 
-	if(eligiblefound && eligiblefound.length) {
-		console.log(eligiblefound[1] + " were eligible")
+	const total = /Total ([0-9]*) plots/
+	const totalfound = log.message.match(total)
+
+	if(eligiblefound && eligiblefound.length && total && totalfound.length) {
+		console.log(eligiblefound[1] + " were eligible, total " + totalfound[1])
 	}
 
 	if(message) {
